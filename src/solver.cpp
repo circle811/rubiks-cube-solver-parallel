522f10953f4bc287e3566cf8c1b1008a05b63c58
Circle <circle811@gmail.com>
2020-09-12 16:35:46 +0800

    modify "src/solver.cpp" add optx_solver opty_solver

--- src/solver.cpp
+++ src/solver.cpp
@@ -18,8 +18,12 @@ constexpr u64 _2p_capacity = 29;
 constexpr u64 opt_capacity = 20;
 
 std::unique_ptr<_2ps_solver> _2p_s = nullptr;
-std::unique_ptr<opt_solver> opt_s = nullptr;
-std::unique_ptr<cuda_cube::opt_solver_manager> opt_s_m = nullptr;
+
+template<typename _opt_solver>
+std::unique_ptr<_opt_solver> opt_s = nullptr;
+
+template<typename _opt_solver, typename _d_opt_solver>
+std::unique_ptr<cuda_cube::g_opt_solver_manager<_opt_solver, _d_opt_solver>> opt_s_m = nullptr;
 
 struct solver_d {
     virtual ~solver_d() = default;
@@ -54,26 +58,27 @@ struct _2p_solver_d : solver_d {
     }
 };
 
-struct opt_solver_d : solver_d {
+template<typename _opt_solver>
+struct g_opt_solver_d : solver_d {
     u64 sym_mask_n_moves;
     u64 n_solution;
 
-    opt_solver_d(u64 n_thread, u64 _sym_mask_n_moves, u64 _n_solution) :
+    g_opt_solver_d(u64 n_thread, u64 _sym_mask_n_moves, u64 _n_solution) :
             sym_mask_n_moves(_sym_mask_n_moves), n_solution(_n_solution) {
-        if (opt_s == nullptr) {
-            opt_s = std::make_unique<opt_solver>(n_thread);
+        if (opt_s<_opt_solver> == nullptr) {
+            opt_s<_opt_solver> = std::make_unique<_opt_solver>(n_thread);
         }
     }
 
-    ~opt_solver_d() override = default;
+    ~g_opt_solver_d() override = default;
 
     std::vector<std::vector<u8>> solve(const cube3 &a) override {
         std::vector<std::vector<u8>> solutions{};
-        auto it = opt_s->solve<opt_capacity>(a, opt_capacity, sym_mask_n_moves);
+        auto it = opt_s<_opt_solver>->template solve<opt_capacity>(a, opt_capacity, sym_mask_n_moves);
         while (solutions.size() < n_solution) {
             auto[f, moves] = it();
             if (f & flag::solution) {
-                std::cout << moves_to_string<opt_solver, opt_capacity>(moves) << std::endl;
+                std::cout << moves_to_string<_opt_solver, opt_capacity>(moves) << std::endl;
                 solutions.emplace_back(moves.a.begin(), moves.a.begin() + moves.n);
             } else if (f & flag::end) {
                 break;
@@ -83,41 +88,42 @@ struct opt_solver_d : solver_d {
     }
 };
 
-struct thread_opt_solver_d : solver_d {
-    typedef thread_dfs<opt_solver, opt_capacity> parallel_dfs;
+template<typename _opt_solver>
+struct g_thread_opt_solver_d : solver_d {
+    typedef thread_dfs<_opt_solver, opt_capacity> parallel_dfs;
 
     const typename parallel_dfs::solver *pd_opt_s;
     std::string schedule;
     u64 n_pd_thread;
     u64 bfs_count;
 
-    thread_opt_solver_d(const std::string &_schedule, u64 n_thread, u64 _bfs_count) :
+    g_thread_opt_solver_d(const std::string &_schedule, u64 n_thread, u64 _bfs_count) :
             pd_opt_s(nullptr), schedule(_schedule), n_pd_thread(n_thread), bfs_count(_bfs_count) {
-        if (opt_s == nullptr) {
-            opt_s = std::make_unique<opt_solver>(n_thread);
+        if (opt_s<_opt_solver> == nullptr) {
+            opt_s<_opt_solver> = std::make_unique<_opt_solver>(n_thread);
         }
-        pd_opt_s = opt_s.get();
+        pd_opt_s = opt_s<_opt_solver>.get();
     }
 
-    ~thread_opt_solver_d() override = default;
+    ~g_thread_opt_solver_d() override = default;
 
     std::vector<std::vector<u8>> solve(const cube3 &a) override {
         u64 f;
         t_moves<opt_capacity> moves;
         if (schedule == "simple") {
-            std::tie(f, moves) = parallel_ida_star<opt_solver, opt_capacity, parallel_dfs, simple_schedule>::run(
-                    *opt_s, *pd_opt_s, a, n_pd_thread, opt_capacity, bfs_count);
+            std::tie(f, moves) = parallel_ida_star<_opt_solver, opt_capacity, parallel_dfs, simple_schedule>::run(
+                    *opt_s<_opt_solver>, *pd_opt_s, a, n_pd_thread, opt_capacity, bfs_count);
         } else if (schedule == "linear") {
-            std::tie(f, moves) = parallel_ida_star<opt_solver, opt_capacity, parallel_dfs, linear_schedule>::run(
-                    *opt_s, *pd_opt_s, a, n_pd_thread, opt_capacity, bfs_count);
+            std::tie(f, moves) = parallel_ida_star<_opt_solver, opt_capacity, parallel_dfs, linear_schedule>::run(
+                    *opt_s<_opt_solver>, *pd_opt_s, a, n_pd_thread, opt_capacity, bfs_count);
         } else if (schedule == "best") {
-            std::tie(f, moves) = parallel_ida_star<opt_solver, opt_capacity, parallel_dfs, best_schedule>::run(
-                    *opt_s, *pd_opt_s, a, n_pd_thread, opt_capacity, bfs_count);
+            std::tie(f, moves) = parallel_ida_star<_opt_solver, opt_capacity, parallel_dfs, best_schedule>::run(
+                    *opt_s<_opt_solver>, *pd_opt_s, a, n_pd_thread, opt_capacity, bfs_count);
         } else {
             assert(0);
         }
         if (f & flag::solution) {
-            std::cout << moves_to_string<opt_solver, opt_capacity>(moves) << std::endl;
+            std::cout << moves_to_string<_opt_solver, opt_capacity>(moves) << std::endl;
             return {std::vector<u8>(moves.a.begin(), moves.a.begin() + moves.n)};
         } else {
             return {};
@@ -125,44 +131,48 @@ struct thread_opt_solver_d : solver_d {
     }
 };
 
-struct cuda_opt_solver_d : solver_d {
-    typedef cuda_cube::cuda_dfs<opt_solver, cuda_cube::opt_solver, opt_capacity> parallel_dfs;
+template<typename _opt_solver, typename _d_opt_solver>
+struct g_cuda_opt_solver_d : solver_d {
+    typedef cuda_cube::cuda_dfs<_opt_solver, _d_opt_solver, opt_capacity> parallel_dfs;
 
     const typename parallel_dfs::solver *pd_opt_s;
     std::string schedule;
     u64 n_pd_thread;
     u64 bfs_count;
 
-    cuda_opt_solver_d(const std::string &_schedule, u64 n_thread, u64 n_cuda_thread, u64 _bfs_count) :
+    g_cuda_opt_solver_d(const std::string &_schedule, u64 n_thread, u64 n_cuda_thread, u64 _bfs_count) :
             pd_opt_s(nullptr), schedule(_schedule), n_pd_thread(n_cuda_thread), bfs_count(_bfs_count) {
-        if (opt_s == nullptr) {
-            opt_s = std::make_unique<opt_solver>(n_thread);
+        if (opt_s<_opt_solver> == nullptr) {
+            opt_s<_opt_solver> = std::make_unique<_opt_solver>(n_thread);
         }
-        if (opt_s_m == nullptr) {
-            opt_s_m = std::make_unique<cuda_cube::opt_solver_manager>(*opt_s);
+        if (opt_s_m<_opt_solver, _d_opt_solver> == nullptr) {
+            opt_s_m<_opt_solver, _d_opt_solver> =
+                    std::make_unique<cuda_cube::g_opt_solver_manager<_opt_solver, _d_opt_solver>>(*opt_s<_opt_solver>);
         }
-        pd_opt_s = &opt_s_m->get();
+        pd_opt_s = &opt_s_m<_opt_solver, _d_opt_solver>->get();
     }
 
-    ~cuda_opt_solver_d() override = default;
+    ~g_cuda_opt_solver_d() override {
+        opt_s_m<_opt_solver, _d_opt_solver> = nullptr;
+    }
 
     std::vector<std::vector<u8>> solve(const cube3 &a) override {
         u64 f;
         t_moves<opt_capacity> moves;
         if (schedule == "simple") {
-            std::tie(f, moves) = parallel_ida_star<opt_solver, opt_capacity, parallel_dfs, simple_schedule>::run(
-                    *opt_s, *pd_opt_s, a, n_pd_thread, opt_capacity, bfs_count);
+            std::tie(f, moves) = parallel_ida_star<_opt_solver, opt_capacity, parallel_dfs, simple_schedule>::run(
+                    *opt_s<_opt_solver>, *pd_opt_s, a, n_pd_thread, opt_capacity, bfs_count);
         } else if (schedule == "linear") {
-            std::tie(f, moves) = parallel_ida_star<opt_solver, opt_capacity, parallel_dfs, linear_schedule>::run(
-                    *opt_s, *pd_opt_s, a, n_pd_thread, opt_capacity, bfs_count);
+            std::tie(f, moves) = parallel_ida_star<_opt_solver, opt_capacity, parallel_dfs, linear_schedule>::run(
+                    *opt_s<_opt_solver>, *pd_opt_s, a, n_pd_thread, opt_capacity, bfs_count);
         } else if (schedule == "best") {
-            std::tie(f, moves) = parallel_ida_star<opt_solver, opt_capacity, parallel_dfs, best_schedule>::run(
-                    *opt_s, *pd_opt_s, a, n_pd_thread, opt_capacity, bfs_count);
+            std::tie(f, moves) = parallel_ida_star<_opt_solver, opt_capacity, parallel_dfs, best_schedule>::run(
+                    *opt_s<_opt_solver>, *pd_opt_s, a, n_pd_thread, opt_capacity, bfs_count);
         } else {
             assert(0);
         }
         if (f & flag::solution) {
-            std::cout << moves_to_string<opt_solver, opt_capacity>(moves) << std::endl;
+            std::cout << moves_to_string<_opt_solver, opt_capacity>(moves) << std::endl;
             return {std::vector<u8>(moves.a.begin(), moves.a.begin() + moves.n)};
         } else {
             return {};
@@ -197,19 +207,28 @@ std::string moves_to_string(const std::vector<u8> &moves) {
     return s;
 }
 
-std::string usage = R"(Usage
+std::string usage = R"(Usage:
 
 Two Phase Solver:
-    %s  --algorithm two_phase  --n_thread 4  --2p_n_moves 24  --input example.txt  --output result.txt
+    %s  --algorithm 2p  --n_thread 4  --2p_n_moves 24  --input example.txt  --output result.txt
 
-Optimum Solver:
-    %s  --algorithm optimum  --n_thread 4  --sym_n_moves 6  --n_solution=1  --input example.txt  --output result.txt
+Optimum X Solver:
+    %s  --algorithm optx  --n_thread 4  --sym_n_moves 6  --n_solution=1  --input example.txt  --output result.txt
 
-Thread Optimum Solver:
-    %s  --algorithm thread_optimum  --schedule simple  --n_thread 4  --bfs_count=100000  --input example.txt  --output result.txt
+Thread Optimum X Solver:
+    %s  --algorithm thread_optx  --schedule simple  --n_thread 4  --bfs_count=100000  --input example.txt  --output result.txt
 
-CUDA Optimum Solver:
-    %s  --algorithm cuda_optimum  --schedule simple  --n_thread 4  --n_cuda_thread 4096  --bfs_count=100000  --input example.txt  --output result.txt
+CUDA Optimum X Solver:
+    %s  --algorithm cuda_optx  --schedule simple  --n_thread 4  --n_cuda_thread 4096  --bfs_count=100000  --input example.txt  --output result.txt
+
+Optimum Y Solver:
+    %s  --algorithm opty  --n_thread 4  --sym_n_moves 6  --n_solution=1  --input example.txt  --output result.txt
+
+Thread Optimum Y Solver:
+    %s  --algorithm thread_opty  --schedule simple  --n_thread 4  --bfs_count=100000  --input example.txt  --output result.txt
+
+CUDA Optimum Y Solver:
+    %s  --algorithm cuda_opty  --schedule simple  --n_thread 4  --n_cuda_thread 4096  --bfs_count=100000  --input example.txt  --output result.txt
 )";
 
 void parse_arg(
@@ -217,7 +236,8 @@ void parse_arg(
         std::string &algorithm, std::string &schedule, u64 &n_thread, u64 &n_cuda_thread,
         u64 &_2p_n_moves, u64 &sym_n_moves, u64 &n_solution, u64 &bfs_count,
         std::string &input, std::string &output) {
-    std::set<std::string> algorithm_set = {"two_phase", "optimum", "thread_optimum", "cuda_optimum"};
+    std::set<std::string> algorithm_set = {
+            "2p", "optx", "thread_optx", "cuda_optx", "opty", "thread_opty", "cuda_opty"};
     std::set<std::string> schedule_set = {"simple", "linear", "best"};
     std::tuple<u64, u64> n_thread_t = {1, 256};
     std::tuple<u64, u64> n_cuda_thread_t = {1, 65536};
@@ -226,11 +246,11 @@ void parse_arg(
     std::tuple<u64, u64> n_solution_t = {1, u64(-1)};
     std::tuple<u64, u64> bfs_count_t = {1, u64(-1)};
 
-    cxxopts::Options option(argv[0], "Parallel Rubik's Cube Solver");
+    cxxopts::Options option(argv[0], "Rubik's Cube Solver (Parallel)");
     option.add_options()
-            ("algorithm", "[two_phase optimum thread_optimum cuda_optimum]",
+            ("algorithm", "(2p | optx | thread_optx | cuda_optx | opty | thread_opty | cuda_opty)",
              cxxopts::value<std::string>(algorithm))
-            ("schedule", "[simple linear best]", cxxopts::value<std::string>(schedule)->default_value("simple"))
+            ("schedule", "(simple | linear | best)", cxxopts::value<std::string>(schedule)->default_value("simple"))
             ("n_thread", "1~256", cxxopts::value<u64>(n_thread)->default_value("4"))
             ("n_cuda_thread", "1~65536", cxxopts::value<u64>(n_cuda_thread)->default_value("4096"))
             ("2p_n_moves", "0~29", cxxopts::value<u64>(_2p_n_moves)->default_value("24"))
@@ -239,22 +259,16 @@ void parse_arg(
             ("bfs_count", "1~max", cxxopts::value<u64>(bfs_count)->default_value("100000"))
             ("input", "input file name", cxxopts::value<std::string>(input)->default_value(""))
             ("output", "output file name", cxxopts::value<std::string>(output)->default_value(""))
-            ("usage", "show usage")
             ("help", "show help");
 
     try {
         cxxopts::ParseResult result = option.parse(argc, argv);
-        if (result.count("usage")) {
-            u64 n = usage.size() + strlen(argv[0]) * 4;
+        if (result.count("help") > 0 or result.count("algorithm") == 0) {
+            std::cout << option.help() << std::endl;
+            u64 n = usage.size() + strlen(argv[0]) * 7;
             std::vector<char> buf(n, '\0');
-            snprintf(&buf[0], n, usage.c_str(), argv[0], argv[0], argv[0], argv[0]);
+            snprintf(&buf[0], n, usage.c_str(), argv[0], argv[0], argv[0], argv[0], argv[0], argv[0], argv[0]);
             std::cout << &buf[0] << std::endl;
-            exit(0);
-        } else if (result.count("help")) {
-            std::cout << option.help() << std::endl;
-            exit(0);
-        } else if (result.count("algorithm") == 0) {
-            std::cout << option.help() << std::endl;
             exit(0);
         }
     } catch (const cxxopts::OptionException &e) {
@@ -333,14 +347,22 @@ int main(int argc, char **argv) {
     std::ostream &out = output.empty() ? std::cout : *out_f;
 
     std::unique_ptr<solver_d> sd = nullptr;
-    if (algorithm == "two_phase") {
+    if (algorithm == "2p") {
         sd = std::make_unique<_2p_solver_d>(n_thread, _2p_n_moves);
-    } else if (algorithm == "optimum") {
-        sd = std::make_unique<opt_solver_d>(n_thread, sym_n_moves, n_solution);
-    } else if (algorithm == "thread_optimum") {
-        sd = std::make_unique<thread_opt_solver_d>(schedule, n_thread, bfs_count);
-    } else if (algorithm == "cuda_optimum") {
-        sd = std::make_unique<cuda_opt_solver_d>(schedule, n_thread, n_cuda_thread, bfs_count);
+    } else if (algorithm == "optx") {
+        sd = std::make_unique<g_opt_solver_d<optx_solver>>(n_thread, sym_n_moves, n_solution);
+    } else if (algorithm == "thread_optx") {
+        sd = std::make_unique<g_thread_opt_solver_d<optx_solver>>(schedule, n_thread, bfs_count);
+    } else if (algorithm == "cuda_optx") {
+        sd = std::make_unique<g_cuda_opt_solver_d<optx_solver, cuda_cube::optx_solver>>(
+                schedule, n_thread, n_cuda_thread, bfs_count);
+    } else if (algorithm == "opty") {
+        sd = std::make_unique<g_opt_solver_d<opty_solver>>(n_thread, sym_n_moves, n_solution);
+    } else if (algorithm == "thread_opty") {
+        sd = std::make_unique<g_thread_opt_solver_d<opty_solver>>(schedule, n_thread, bfs_count);
+    } else if (algorithm == "cuda_opty") {
+        sd = std::make_unique<g_cuda_opt_solver_d<opty_solver, cuda_cube::opty_solver>>(
+                schedule, n_thread, n_cuda_thread, bfs_count);
     } else {
         assert(0);
     }
